35d7e3c5070b63d8e07ca93a4b7ff80e9056c238
Pratham Rawat <prathamrawat2004@gmail.com>
2025-03-06 20:25:00 -0800

    BinarySearch - rev

---
+++ Java Programs/src/BinarySearch/Algorithm.java
@@ -0,0 +1,32 @@
+package BinarySearch;
+import java.util.*;
+
+// considering the arraylist is SORTED & contains no DUPLICATE elements
+public class Algorithm {
+    public int BinarySearch(ArrayList<Integer> A, int B){
+        int low = 0, high = A.size() - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if(A.get(mid) == B){
+                // no duplicate elements, hence there will be only one element
+                // if arraylist had duplicate then shrink the size by high = mid - 1
+
+                return mid; 
+
+            }else if(A.get(mid) < B){
+                // shrink the size to greater as the list is sorted
+                low = mid + 1;
+
+            }else{
+                // shrink the size to smaller part as list is sorted
+                high = mid - 1;
+            }
+        }
+
+        // if the target is not found
+        return -1;
+
+    }
+}
---
+++ Java Programs/src/BinarySearch/CountElementOccurence.java
@@ -0,0 +1,71 @@
+package BinarySearch;
+
+import java.util.*;
+
+public class CountElementOccurence {
+    public int findCount(final List<Integer> A, int B) {
+        // getting the starting index of the req element
+        int start = findStart(A, B);
+        int end = findEnd(A, B);
+
+        // counting the number of occurences
+        int occurences = (end - start) + 1;
+
+        return occurences;
+
+    }
+
+    // helper function to get the starting index
+    public int findStart(List<Integer> A, int B) {
+        int low = 0, high = A.size() - 1;
+        int start = -1;
+
+        // binary search
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (A.get(mid) == B) {
+                // got the initial start
+                start = mid;
+
+                // checking if start could be found on the left side also
+                high = mid - 1;
+
+            } else if (A.get(mid) < B) {
+                low = mid + 1;
+
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return start;
+    }
+
+    // helper function to get the end index
+    public int findEnd(List<Integer> A, int B) {
+        int low = 0, high = A.size() - 1;
+        int end = -2; // so that occurences could become 0 in case no element found
+
+        // binary search
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (A.get(mid) == B) {
+                // got the initial start
+                end = mid;
+
+                // checking if start could be found on the left side also
+                low = mid + 1;
+
+            } else if (A.get(mid) < B) {
+                low = mid + 1;
+
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return end;
+    }
+}
