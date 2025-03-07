d990e77180777fb32af987b2418f1524aaf4c58c
Pratham Rawat <prathamrawat2004@gmail.com>
2025-03-06 20:25:31 -0800

    BS - rev

--- Java Programs/src/BinarySearch/Algorithm.java
+++ Java Programs/src/BinarySearch/Algorithm.java
@@ -1,25 +1,26 @@
 package BinarySearch;
+
 import java.util.*;
 
 // considering the arraylist is SORTED & contains no DUPLICATE elements
 public class Algorithm {
-    public int BinarySearch(ArrayList<Integer> A, int B){
+    public int BinarySearch(ArrayList<Integer> A, int B) {
         int low = 0, high = A.size() - 1;
 
         while (low <= high) {
             int mid = low + (high - low) / 2;
 
-            if(A.get(mid) == B){
+            if (A.get(mid) == B) {
                 // no duplicate elements, hence there will be only one element
                 // if arraylist had duplicate then shrink the size by high = mid - 1
 
-                return mid; 
+                return mid;
 
-            }else if(A.get(mid) < B){
+            } else if (A.get(mid) < B) {
                 // shrink the size to greater as the list is sorted
                 low = mid + 1;
 
-            }else{
+            } else {
                 // shrink the size to smaller part as list is sorted
                 high = mid - 1;
             }
