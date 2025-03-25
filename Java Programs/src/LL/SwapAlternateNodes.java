f4c384f323bbda0af4c453bb264e006bb18ada1d
Pratham Rawat <prathamrawat2004@gmail.com>
2025-03-24 20:31:20 -0700

    rev done

---
+++ Java Programs/src/LL/SwapAlternateNodes.java
@@ -0,0 +1,39 @@
+package LL;
+
+public class SwapAlternateNodes {
+    class ListNode {
+        public int val;
+        public ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public ListNode swapPairs(ListNode A) {
+        // base condition
+        if (A.next == null)
+            return A; // only one node
+
+        // declaring pointers
+        ListNode dummy = new ListNode(0);
+        ListNode prev = dummy, curr = A;
+
+        while (curr != null && curr.next != null) {
+            ListNode first = curr;
+            ListNode second = curr.next;
+
+            // swapping the nodes
+            prev.next = second;
+            first.next = second.next;
+            second.next = first;
+
+            // moving the pointers
+            prev = first;
+            curr = first.next;
+        }
+
+        return dummy.next;
+    }
+}
