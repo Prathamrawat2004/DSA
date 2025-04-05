b89228913149a27d4d65fd71a4a22c1c09c6ca09
Pratham Rawat <prathamrawat2004@gmail.com>
2025-04-05 01:07:50 -0700

    rev done

---
+++ Java Programs/src/BinarySearch/SearchARange.java
@@ -0,0 +1,57 @@
+package BinarySearch;
+
+import java.util.*;
+
+public class SearchARange {
+    public ArrayList<Integer> searchRange(ArrayList<Integer> A, int B) {
+        // resultant arraylist
+        ArrayList<Integer> resultant = new ArrayList<>();
+        resultant.add(findStart(A, B));
+        resultant.add(findEnd(A, B));
+        return resultant;
+    }
+
+    // function to return start
+    public int findStart(ArrayList<Integer> A, int B) {
+        // using binary search
+        int low = 0, high = A.size() - 1;
+        int start = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (A.get(mid) == B) {
+                start = mid;
+                high = mid - 1; // search left
+            } else if (A.get(mid) < B) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+
+        }
+
+        return start;
+    }
+
+    // function to return end
+    public int findStart(ArrayList<Integer> A, int B) {
+        // using binary search
+        int low = 0, high = A.size() - 1;
+        int end = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (A.get(mid) == B) {
+                end = mid;
+                low = mid + 1; // search right
+            } else if (A.get(mid) < B) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+
+        }
+
+        return end;
+    }
+}
---
+++ Java Programs/src/LL/Reverse2.java
@@ -0,0 +1,64 @@
+package LL;
+
+import java.util.*;
+
+public class Reverse2 {
+    class ListNode {
+        public int val;
+        public ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public ListNode reverseBetween(ListNode A, int B, int C) {
+        // base condition
+        if (A == null || A.next == null || B == C) {
+            return A;
+        }
+
+        // creating a dummy node
+        ListNode dummy = new ListNode(0);
+        dummy.next = A;
+
+        // starting from dummy node
+        ListNode temp = dummy;
+
+        // setting the starting parts
+        // moving till (B - 1)th node
+        for (int i = 1; i < B; i++) {
+            temp = temp.next;
+        }
+
+        ListNode low = temp;
+        ListNode start = low.next;
+
+        // setting the ending parts
+        // moving till Cth node
+        temp = start;
+        for (int j = B; j < C; j++) {
+            temp = temp.next;
+        }
+
+        ListNode high = temp.next;
+
+        // reversing the nodes
+        ListNode prev = null;
+        ListNode curr = start;
+        while (curr != high) {
+            ListNode nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+
+        // attaching with original lists
+        low.next = prev;
+        start.next = high;
+
+        return dummy.next;
+
+    }
+}
