140a71604218aa3bfeb51eab955fbeeb06261b92
Pratham Rawat <prathamrawat2004@gmail.com>
2025-04-15 01:22:31 -0700

    rev done

---
+++ Java Programs/src/Sorting/Bubble.java
@@ -0,0 +1,28 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class Bubble {
+    public void sort(int[] arr, int n) {
+        // no. of passes
+        for (int i = 0; i < n - 1; i++) {
+            boolean swapped = false; // for stopping the loop if no swapping in any pass
+
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            // if at any pass no swapping is done
+            if (!swapped) {
+                break;
+            }
+
+        }
+    }
+}
---
+++ Java Programs/src/Sorting/Kadane's.java
@@ -0,0 +1,18 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class Kadane's {
+    public int Kadane(int[] arr){
+        // pointer for recording the max sum
+        int max_Sum_So_Far = arr[0];
+        int max_Ending_Here = arr[0];
+
+        for(int i = 1; i < arr.length; i++){
+            max_Ending_Here = Math.max(arr[i], max_Ending_Here + arr[i]);
+            max_Sum_So_Far = Math.max(max_Ending_Here, max_Sum_So_Far);
+        }
+
+        return max_Sum_So_Far;
+    }
+}
