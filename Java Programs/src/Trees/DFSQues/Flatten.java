70d3a12ccb8a68103ad7609027058cf586e8e4c6
Pratham Rawat <prathamrawat2004@gmail.com>
2025-04-28 03:09:31 -0700

    rev done

--- Java Programs/src/Trees/DFSQues/Flatten.java
+++ Java Programs/src/Trees/DFSQues/Flatten.java
@@ -1,4 +1,9 @@
+// Done
+
 package Trees.DFSQues;
+
+import java.util.LinkedList;
+import java.util.*;
 
 public class Flatten {
     public class TreeNode {
@@ -20,8 +25,8 @@ public class Flatten {
         }
 
     }
-    
 
+    // optimized apporach
     public void flatten(TreeNode root) {
         TreeNode current = root;
         while (current != null) {
@@ -39,5 +44,46 @@ public class Flatten {
 
             current = current.right;
         }
+    }
+
+    // brute force
+    public void Flatten(TreeNode root) {
+        // base condition
+        if (root == null) {
+            return;
+        }
+
+        // implementing queue for dfs
+        Queue<TreeNode> queue = new LinkedList<>();
+
+        // helper function
+        dfs(root, queue);
+
+        // creating a linkedlist
+        TreeNode prev = queue.poll();
+        while (!queue.isEmpty()) {
+            TreeNode curr = queue.poll();
+            prev.left = null;
+            prev.right = curr;
+            prev = curr;
+        }
+
+    }
+
+    // helper function definition
+    public void dfs(TreeNode node, Queue<TreeNode> queue) {
+        // base condition
+        if (node == null) {
+            return;
+        }
+
+        // add current node
+        queue.offer(node);
+
+        // go to left
+        dfs(node.left, queue);
+
+        // go to right
+        dfs(node.right, queue);
     }
 }
