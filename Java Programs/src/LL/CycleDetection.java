2252cf20251db9035aa217ed2ec2ab6a6c45aa71
Pratham Rawat <prathamrawat2004@gmail.com>
2025-07-25 17:54:35 +0530

    rev done

--- Java Programs/src/LL/CycleDetection.java
+++ Java Programs/src/LL/CycleDetection.java
@@ -13,6 +13,30 @@ public class CycleDetection {
         }
     }
 
+    // brute force approach
+    // as space complexity = O(n)
+    public ListNode detectCycle1(ListNode head){
+        // using hashset to store the listnodes
+        HashSet<ListNode> uniqueNodes = new HashSet<>();
+        ListNode current = head;
+
+        while(current != null){
+            // checking if already visited or not
+            if(uniqueNodes.contains(current)){
+                return current;
+            }
+
+            uniqueNodes.add(current);
+            current = current.next;
+
+        }
+
+        return null; // no cycle found
+
+    }
+
+    // optimized approach
+    // as space complexity = O(1)
     public ListNode detectCycle(ListNode head) {
         // base condition
         if (head == null || head.next == null) {
