56afd2c3cdd0c1cfa0178a02a0d9558a9d91c9e3
Pratham Rawat <prathamrawat2004@gmail.com>
2025-08-03 21:06:36 +0530

    rev done

--- Java Programs/src/TwoPointer/MaximumOnes.java
+++ Java Programs/src/TwoPointer/MaximumOnes.java
@@ -1,8 +1,34 @@
 package TwoPointer;
 
 import java.util.*;
-
+// revisions = 1
 public class MaximumOnes {
+    // brute force approach
+    public int solve1(ArrayList<Integer> A, int B) {
+        int n = A.size();
+        int maxLength = 0;
+
+        for (int i = 0; i < n; i++) {
+            int zeroCount = 0;
+
+            for (int j = i; j < n; j++) {
+                if (A.get(j).equals(0)) {
+                    zeroCount++;
+                }
+
+                if (zeroCount > B) {
+                    break;
+                }
+
+                // keep updating maxlength
+                maxLength = Math.max(maxLength, j - i + 1);
+            }
+        }
+
+        return maxLength;
+    }
+
+    // optimized approach
     public int solve(ArrayList<Integer> A, int B) {
         // req pointers
         int left = 0, right = 0;
--- Java Programs/src/TwoPointer/MaximumOnes2.java
+++ Java Programs/src/TwoPointer/MaximumOnes2.java
@@ -1,12 +1,46 @@
 package TwoPointer;
 
 import java.util.ArrayList;
-
+// revisions = 1
 public class MaximumOnes2 {
-    public ArrayList<Integer> maxOnes(ArrayList<Integer> A, int B) {
-        // resultant array
-        ArrayList<Integer> resultant = new ArrayList<>();
+    // brute force approach
+    public ArrayList<Integer> maxOnes1(ArrayList<Integer> A, int B) {
+        int n = A.size();
+        int bestStart = -1; // no best start initially
+        int maxLength = 0;
 
+        for (int i = 0; i < n; i++) {
+
+            int zeroCount = 0;
+
+            for (int j = i; j < n; j++) {
+                if (A.get(j).equals(0)) {
+                    zeroCount++;
+                }
+
+                if (zeroCount > B) {
+                    break;
+                }
+
+                // keep updating maxlength & best starting index
+                if ((j - i + 1) > maxLength) {
+                    maxLength = j - i + 1;
+                    bestStart = i;
+                }
+            }
+        }
+
+        // build the resultant list
+        ArrayList<Integer> resultant = new ArrayList<>();
+        for (int k = bestStart; k < bestStart + maxLength; k++) {
+            resultant.add(k);
+        }
+
+        return resultant;
+    }
+
+    // optimized approach
+    public ArrayList<Integer> maxOnes(ArrayList<Integer> A, int B) {
         // declaring pointers for sliding window
         int left = 0, zeroCount = 0;
         int bestLeft = 0; // for best starting index
@@ -19,7 +53,7 @@ public class MaximumOnes2 {
                 zeroCount++;
             }
 
-            while (zeroCount > B) {
+            while (zeroCount > B && left <= right) {
                 if (A.get(left) == 0) {
                     zeroCount--;
                 }
@@ -32,6 +66,9 @@ public class MaximumOnes2 {
                 bestLeft = left;
             }
         }
+
+        // resultant array
+        ArrayList<Integer> resultant = new ArrayList<>();
 
         // adding the indexes to the resultant list
         for (int i = bestLeft; i < bestLeft + maxLength; i++) {
